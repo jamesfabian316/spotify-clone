ce27e2c6cd64f84ecee1406e03cccd30642d0a8d
James Fabian <90632165+jamesfabian316@users.noreply.github.com>
2024-06-03 15:13:19 +0530

    chore: Ignore password.txt in .gitignore, remove .env.example, and optimize SupabaseProvider and RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,34 +14,28 @@ import './globals.css'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Spotify Clone',
+	title: 'Spotify Clone',
+	description: 'Spotify Clone',
 }
 
-export const revalidate = 0;
+export const revalidate = 0
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  const products = await getActiveProductsWithPrices();
-  const userSongs = await getSongsByUserId();
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+	const products = await getActiveProductsWithPrices()
+	const userSongs = await getSongsByUserId()
 
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <ToasterProvider />
-        <SupabaseProvider>
-          <UserProvider>
-            <ModalProvider products={products} />
-            <Sidebar songs={userSongs}>
-              {children}
-            </Sidebar>
-            <Player />
-          </UserProvider>
-        </SupabaseProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={font.className}>
+				<ToasterProvider />
+				<SupabaseProvider>
+					<UserProvider>
+						<ModalProvider products={products} />
+						<Sidebar songs={userSongs}>{children}</Sidebar>
+						<Player />
+					</UserProvider>
+				</SupabaseProvider>
+			</body>
+		</html>
+	)
 }
